e8365f03b9ee331a09305b805dcd2162ccaa606b
liangxinmeng <liangxinmeng@126.com>
2017-04-09 17:43:18 +0800

    play

--- app/src/main/java/com/austin/audio/MainActivity.java
+++ app/src/main/java/com/austin/audio/MainActivity.java
@@ -1,22 +1,33 @@
 package com.austin.audio;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTv;
     private RecordButton mRecordButton;
     private boolean flag;
+    private CheckBox radioButton;
+    private AudioManager audioManager;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        radioButton = (CheckBox) findViewById(R.id.radioButton);
 
         mRecordButton = (RecordButton) findViewById(R.id.recordButton);
 
@@ -39,5 +50,42 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        radioButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_MUTE, AudioManager.FLAG_SHOW_UI);
+
+                }else{
+                    audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_UNMUTE, AudioManager.FLAG_SHOW_UI);
+
+                }
+            }
+        });
+
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+
+
     }
+
+
+    public void play(View view) throws IOException {
+        if(mediaPlayer==null)
+        mediaPlayer = MediaPlayer.create(this, R.raw.lightning);
+
+        mediaPlayer.seekTo(0);
+        mediaPlayer.start();
+    }
+
+    public void up(View view) {
+        audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+
+    }
+
+    public void down(View view) {
+        audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
+
+    }
+
 }
